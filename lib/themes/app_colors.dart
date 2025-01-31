449cef28448aff795922bf4158d2d0e07cea1cc6
rivo pelu <dusty.emit@finestudio.org>
2025-01-31 17:47:47 +0700

    FEAT:setup home page

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,8 @@
+
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryMain = Color(0xFF1B769F);
+  static const Color primaryDark = Color(0xFF044E70);
+  static const Color primaryLight = Color(0xFF51AAD2);
+}
